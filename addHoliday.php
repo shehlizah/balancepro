8f8e0b11f6fee448bccd16742fe4167460c2ab67
Shehlizah <shehlizah1@gmail.com>
2024-11-04 22:43:34 +0500

    addHoliday Changed

--- addHoliday.php
+++ addHoliday.php
@@ -1,43 +1,33 @@
 <?php 
-ini_set('display_error',1);
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-    global $wpdb;
-	require_once dirname( __DIR__ ) . '/wp-load.php';
+global $wpdb;
+require_once dirname(__DIR__) . '/wp-load.php';
 
-    
-    function get_holiday($wpdb){
-        $chat_holiday = $wpdb->get_results("select chat_id FROM wp_chat_setting where chat_value LIKE '%toDatef%'" );
+$insert_data = serialize($_POST);
 
-        if(!$chat_holiday[0])
-		    return -1;
-
-		return $chat_holiday;
-	}
-
-	$chat_id = get_holiday($wpdb);
-    // logging();
-    print($chat_id);
-	var_dump($chat_id);
-    $query = "UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id=$chat_id";
-    var_dump($query);
-    print_r($query);
-
-	$insert_data = serialize($_POST);
-
-	//holidaysData
-    if($chat_id!=-1){
-    	// $sql = $wpdb->prepare("UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id=1");
-		$sql = $wpdb->prepare("UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id='$chat_id'");
-        print($sql);
-        
-		$wpdb->query($sql);
-	}
-    else {
-    	$sql = $wpdb->prepare("INSERT INTO `wp_chat_setting` (`chat_value`) values ('$insert_data')");
-		$wpdb->query($sql);
-
+function get_holiday($wpdb) {
+    $chat_holiday = $wpdb->get_results("SELECT chat_id FROM wp_chat_setting WHERE chat_value LIKE '%toDatef%'");
+    print_r($chat_holiday);
+    if (empty($chat_holiday)) {
+        return -1;
     }
-//print_r($insert_data);
-header("location: chat.php");exit;
+
+    return $chat_holiday[0]->chat_id;
+}
+
+$chat_id = get_holiday($wpdb);
+echo $chat_id;
+
+if ($chat_id != -1) {
+    $sql = $wpdb->prepare("UPDATE wp_chat_setting SET chat_value = %s WHERE chat_id = %d", $insert_data, $chat_id);
+} else {
+    $sql = $wpdb->prepare("INSERT INTO wp_chat_setting (chat_value) VALUES (%s)", $insert_data);
+}
+
+$wpdb->query($sql);
+
+header("Location: chat.php");
+exit;
 ?>
