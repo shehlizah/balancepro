ab9b3c32fe3422097e8866c324b55cac6afaf87c
Shehlizah <shehlizah1@gmail.com>
2024-11-04 21:01:14 +0500

    updated for db

--- addHoliday.php
+++ addHoliday.php
@@ -2,16 +2,35 @@
 ini_set('display_error',1);
 error_reporting(E_ALL);
 
-global $wpdb;
+    global $wpdb;
 	require_once dirname( __DIR__ ) . '/wp-load.php';
 
-	$wp_chat_data = $wpdb->get_results( "SELECT * FROM wp_chat_setting" );
+    
+    function get_holiday($wpdb){
+        $chat_holiday = $wpdb->get_results("select chat_id FROM wp_chat_setting where chat_value LIKE '%toDatef%'" );
+
+        if(!$chat_holiday[0])
+		    return -1;
+
+		return $chat_holiday;
+	}
+
+	$chat_id = get_holiday($wpdb);
+    // logging();
+    print($chat_id);
+	var_dump($chat_id);
+    $query = "UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id=$chat_id";
+    var_dump($query);
+    print_r($query);
+
 	$insert_data = serialize($_POST);
 
 	//holidaysData
-	if(!empty($wp_chat_data[0])){
-		// $sql = $wpdb->prepare("UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id=1");
-		$sql = $wpdb->prepare("UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id=1");
+    if($chat_id!=-1){
+    	// $sql = $wpdb->prepare("UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id=1");
+		$sql = $wpdb->prepare("UPDATE `wp_chat_setting` SET `chat_value` ='$insert_data' where chat_id='$chat_id'");
+        print($sql);
+        
 		$wpdb->query($sql);
 	}
     else {
