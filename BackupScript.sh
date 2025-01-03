906b8745507a768a5447ae8c05fe82c990c678ae
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-03 18:56:54 +0500

    Update BackupScript.sh

--- BackupScript.sh
+++ BackupScript.sh
@@ -27,8 +27,8 @@ LS_BACKUP_FOLDER="$PWD/ls_backup_$TIMESTAMP"
 
 
 #WL
-LS_RNDR_SEARCH="${TMP_SOURCE_DIR}render-search-main-design.php"
-CSS_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css"
+#LS_RNDR_SEARCH="${TMP_SOURCE_DIR}render-search-main-design.php"
+#CSS_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css"
 IND_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/index.php"
 CONF_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/includes/config.php"
 LIFESTAGE="${TMP_SOURCE_DIR}lifestage.php"
@@ -40,8 +40,8 @@ LS_CORE="${INC_SOURCE_DIR}core.php"
 
 
 #balanceproMain
-MAIN_CSS_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
-MAIN_RNDR_SEARCH="${MAIN_RNDR_SOURCE_DIR}render-search-main-design.php"
+#MAIN_CSS_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
+#MAIN_RNDR_SEARCH="${MAIN_RNDR_SOURCE_DIR}render-search-main-design.php"
 LS_MAIN_SQ_PG="${LS_MAIN_SRC_DIR}search_query_pagination.php"
 LS_MAIN_RMRS="${MAIN_RNDR_SOURCE_DIR}render-module-M14-15-resources-search.php"
 LS_MAIN_RSMDL="${MAIN_RNDR_SOURCE_DIR}render-search-main-design-lifestage.php"
@@ -59,7 +59,7 @@ echo "MAIN Backup folder created at: $LS_MAIN_BACKUP_FOLDER"
 mkdir -p "$LS_BACKUP_FOLDER"
 echo "WL Backup folder created at: $LS_BACKUP_FOLDER"
 # Create the necessary directories for Whitelabel files
-mkdir -p "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css"
+#mkdir -p "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css"
 mkdir -p "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html"
 mkdir -p "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates"
 mkdir -p "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes"
@@ -69,7 +69,7 @@ mkdir -p "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_
 
 # Create the necessary directories for BalancePro files
 
-mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css"
+#mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css"
 mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/includes/core"
 mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules"
 
@@ -94,9 +94,9 @@ echo "Directories created successfully."
 # #   exit 1
 # # fi
 
-cp "$CSS_DIR" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css/"
-cp "$CSS_DIR" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/"
-cp "$IND_DIR" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/"
+#cp "$CSS_DIR" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css/"
+cp "$IND_DIR" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/"
+cp "$CONF_DIR" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/"
 cp "$LIFESTAGE" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/"
 cp "$LS_RNDR_SEARCH" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/"
 cp "$LS" "$LS_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/"
