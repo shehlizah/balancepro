f6827112a2910e5ff6b4ed569dab9809b6b10582
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-03 19:02:02 +0500

    Update updateFiles.sh

--- updateFiles.sh
+++ updateFiles.sh
@@ -23,9 +23,9 @@ LS_CLONE_FOLDER="$PWD/WL_repo_${LS_BRANCH_NAME}"
 
 #WL
 IND_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/index.php"
-CSS_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css"
+#CSS_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css"
 CONF_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/includes/config.php"
-LS_RNDR_SEARCH="${TMP_SOURCE_DIR}render-search-main-design.php"
+#LS_RNDR_SEARCH="${TMP_SOURCE_DIR}render-search-main-design.php"
 LIFESTAGE="${TMP_SOURCE_DIR}lifestage.php"
 LS="${TMP_SOURCE_DIR}ls.php"
 LS_SQP="${INC_SOURCE_DIR}search_query_pagination.php"
@@ -38,15 +38,15 @@ LS_CORE="${INC_SOURCE_DIR}core.php"
 
 
 #balanceproMain
-MAIN_CSS_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
-MAIN_RNDR_SEARCH="${MAIN_RNDR_SOURCE_DIR}render-search-main-design.php"
+#MAIN_CSS_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
+#MAIN_RNDR_SEARCH="${MAIN_RNDR_SOURCE_DIR}render-search-main-design.php"
 LS_MAIN_SQ_PG="${LS_MAIN_SRC_DIR}search_query_pagination.php"
 LS_MAIN_RMRS="${MAIN_RNDR_SOURCE_DIR}render-module-M14-15-resources-search.php"
 LS_MAIN_RSMDL="${MAIN_RNDR_SOURCE_DIR}render-search-main-design-lifestage.php"
 echo "Copying MAIN updated files"
 
-cp "$LS_MAIN_CLONE_FOLDER/main.min_new.css" "$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
-cp "$LS_MAIN_CLONE_FOLDER/render-search-main-design.php" "$MAIN_RNDR_SEARCH"
+#cp "$LS_MAIN_CLONE_FOLDER/main.min_new.css" "$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
+#cp "$LS_MAIN_CLONE_FOLDER/render-search-main-design.php" "$MAIN_RNDR_SEARCH"
 cp "$LS_MAIN_CLONE_FOLDER/search_query_pagination.php" "$LS_MAIN_SQ_PG" 
 cp "$LS_MAIN_CLONE_FOLDER/render-module-M14-15-resources-search.php" "$LS_MAIN_SQ_PG" 
 cp "$LS_MAIN_CLONE_FOLDER/render-search-main-design-lifestage.php" "$LS_MAIN_SQ_PG" 
@@ -55,9 +55,9 @@ cp "$LS_MAIN_CLONE_FOLDER/render-search-main-design-lifestage.php" "$LS_MAIN_SQ_
 
 echo "Copying WL files"
 cp "$LS_CLONE_FOLDER/index.php" "$HOME/domains/whitelabel.balancepro.org/public_html/index.php"
-cp "$LS_CLONE_FOLDER/main.min.css" "$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" 
+#cp "$LS_CLONE_FOLDER/main.min.css" "$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" 
 cp "$LS_CLONE_FOLDER/config.php" "$HOME/domains/whitelabel.balancepro.org/public_html/includes/config.php"
-cp "$LS_CLONE_FOLDER/render-search-main-design.php" "$LS_RNDR_SEARCH"
+#cp "$LS_CLONE_FOLDER/render-search-main-design.php" "$LS_RNDR_SEARCH"
 cp "$LS_CLONE_FOLDER/search_query_pagination.php" "$LIFESTAGE" 
 cp "$LS_CLONE_FOLDER/lifestage.php" "$WL_SQP"
 cp "$LS_CLONE_FOLDER/ls.php" "$LS"
