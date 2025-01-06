677ab1af7fd82ca58002a1ee257632fc02966e89
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-06 21:51:20 +0500

    Update Searchrestore.sh

--- search-scripts/Searchrestore.sh
+++ search-scripts/Searchrestore.sh
@@ -15,22 +15,19 @@ if [[ "$USER_RESPONSE" != "yes" ]]; then
 fi
 
 # Automatically detect the latest timestamped backup folders in the current working directory
-SEARCH_MAIN_BACKUP_FOLDER=$(ls -d $PWD/search_main_backup_* 2>/dev/null | sort | tail -n 1)
-SEARCH_WL_BACKUP_FOLDER=$(ls -d $PWD/search_wl_backup_* 2>/dev/null | sort | tail -n 1)
+SEARCH_FINAL_BACKUP_FOLDER=$(ls -d $PWD/search_final_backup_* 2>/dev/null | sort | tail -n 1)
+
 
 # Check if the directories exist
-if [ -z "$SEARCH_MAIN_BACKUP_FOLDER" ]; then
-  echo "Error: No MAIN BACKUP directory found in the current working directory."
+if [ -z "$SEARCH_FINAL_BACKUP_FOLDER" ]; then
+  echo "Error: No SEARCH FINAL BACKUP directory found in the current working directory."
   exit 1
 fi
 
-if [ -z "$SEARCH_WL_BACKUP_FOLDER" ]; then
-  echo "Error: No WhiteLabel BACKUP directory found in the current working directory."
-  exit 1
-fi
 
-echo "Detected MAIN BACKUP folder: $SEARCH_MAIN_BACKUP_FOLDER"
-echo "Detected WL BACKUP folder: $SEARCH_WL_BACKUP_FOLDER"
+
+echo "Detected MAIN BACKUP folder: $SEARCH_FINAL_BACKUP_FOLDER"
+
 
 # File paths for restoration (WhiteLabel)
 
@@ -54,19 +51,17 @@ cp() {
   fi
 }
 
-# Restore WhiteLabel files
+# Copy files from the WhiteLabel backup
 echo "Restoring WhiteLabel files..."
-cp  "$SEARCH_WL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/" "$LS_RNDR_SEARCH"
-cp "$SEARCH_WL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" "$CSS_DIR"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" "$CSS_DIR"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/render-search-main-design.php" "$LS_RNDR_SEARCH"
 
 
-echo "Done restoring WhiteLabel files."
-
-# Restore Main website files
-echo "Restoring Main website files..."
-cp "$SEARCH_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/" "$MAIN_CSS_DIR"
-cp "$SEARCH_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/" "$MAIN_RNDR_SEARCH" 
-cp "$SEARCH_MAIN_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/" "$MAIN_MDL_ADMIN"
+# MAIN files to restore
+echo "Restoring MAIN files..."
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css" "$MAIN_CSS_DIR"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-search-main-design.php" "$MAIN_RNDR_SEARCH"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/edit-module-white-label-website-admins.php" "$MAIN_MDL_ADMIN"
 
 
 echo "Done restoring Main website files."
