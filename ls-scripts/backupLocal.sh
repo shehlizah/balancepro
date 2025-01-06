0a18257a8ffad04f2a126370c8a5447d22aaabf0
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-07 00:16:04 +0500

    Update backupLocal.sh

--- ls-scripts/backupLocal.sh
+++ ls-scripts/backupLocal.sh
@@ -12,13 +12,13 @@ INC_SOURCE_DIR="$HOME_WL/includes/core/"    #other all
 MAIN_RNDR_SOURCE_DIR="$HOME_MAIN/wp-content/themes/balance-theme/inc/renderers/modules/"
 LS_MAIN_SRC_DIR="$HOME_MAIN/includes/core/"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")  # Current timestamp for unique backups
-LS_MAIN_BACKUP_FOLDER="$PWD/main_backup_$TIMESTAMP"
-LS_BACKUP_FOLDER="$PWD/wl_backup_$TIMESTAMP"
+LS_FINAL_BACKUP_FOLDER="$PWD/ls_final_backup_$TIMESTAMP"
+
 
 # WL Variables
 #LS_RNDR_SEARCH="${TMP_SOURCE_DIR}render-search-main-design.php"
 #CSS_DIR="$HOME_WL/public_html/assets/css/main.min.css"
-IND_DIR="$HOME_WL/public_html/includes/index.php"
+IND_DIR="$HOME_WL/public_html/index.php"
 CONF_DIR="$HOME_WL/includes/config.php"
 LIFESTAGE="${TMP_SOURCE_DIR}lifestage.php"
 LS="${TMP_SOURCE_DIR}ls.php"
@@ -34,43 +34,40 @@ LS_MAIN_RMRS="${MAIN_RNDR_SOURCE_DIR}render-module-M14-15-resources-search.php"
 LS_MAIN_RSMDL="${MAIN_RNDR_SOURCE_DIR}render-search-main-design-lifestage.php"
 
 # Create the backup folder if it doesn't exist
-mkdir -p "$LS_MAIN_BACKUP_FOLDER"
-echo "MAIN Backup folder created at: $LS_MAIN_BACKUP_FOLDER"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER"
+echo "LS Final Backup folder created at: $LS_FINAL_BACKUP_FOLDER"
 
-mkdir -p "$LS_BACKUP_FOLDER"
-echo "WL Backup folder created at: $LS_BACKUP_FOLDER"
+
 
 # Ensure all necessary directories exist in the backup paths
 #mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/css"
-mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/includes/core"
-mkdir -p "$LS_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/inc/renderers/modules"
-mkdir -p "$LS_BACKUP_FOLDER/whitelabel/public_html/includes/core"
-mkdir -p "$LS_BACKUP_FOLDER/whitelabel/public_html"
-mkdir -p "$LS_BACKUP_FOLDER/whitelabel/public_html/templates"
-mkdir -p "$LS_BACKUP_FOLDER/whitelabel/public_html/includes"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/includes/core"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates"
+mkdir -p "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes"
 echo "Created necessary directories under backup folders."
 
 
 
 
 # Copy files to the correct destination directories
-#cp "$CSS_DIR" "$LS_BACKUP_FOLDER/public_html/assets/css/"
-cp "$IND_DIR" "$LS_BACKUP_FOLDER/public_html/"
-cp "$CONF_DIR" "$LS_BACKUP_FOLDER/whitelabel/public_html/includes/"
-#cp "$LS_RNDR_SEARCH" "$LS_BACKUP_FOLDER/whitelabel/public_html/templates/"
-cp "$LIFESTAGE" "$LS_BACKUP_FOLDER/whitelabel/public_html/templates/"
-cp "$LS" "$LS_BACKUP_FOLDER/whitelabel/public_html/templates/"
-cp "$LS_SQP" "$LS_BACKUP_FOLDER/whitelabel/public_html/includes/core/"
-cp "$LS_RS_SEARCH_PG_CONTENT" "$LS_BACKUP_FOLDER/whitelabel/public_html/includes/core/"
-cp "$LS_CORE" "$LS_BACKUP_FOLDER/whitelabel/public_html/includes/core/"
+
+cp "$IND_DIR" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/"
+cp "$CONF_DIR" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/"
+cp "$LIFESTAGE" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/"
+cp "$LS" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/"
+cp "$LS_SQP" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/"
+cp "$LS_RS_SEARCH_PG_CONTENT" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/"
+cp "$LS_CORE" "$LS_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/"
 
 
 # Main website files copying
-#cp "$MAIN_CSS_DIR" "$LS_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/css/"
-#cp "$MAIN_RNDR_SEARCH" "$LS_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/inc/renderers/modules/"
-cp "$LS_MAIN_SQ_PG" "$LS_MAIN_BACKUP_FOLDER/balancepro/includes/core/"
-cp "$LS_MAIN_RMRS" "$LS_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/inc/renderers/modules/" 
-cp "$LS_MAIN_RSMDL" "$LS_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/inc/renderers/modules/" 
+
+cp "$LS_MAIN_SQ_PG" "$LS_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/includes/core/"
+cp "$LS_MAIN_RMRS" "$LS_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/" 
+cp "$LS_MAIN_RSMDL" "$LS_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/" 
 
 
 echo "Files copied successfully!"
