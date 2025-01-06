0c26eec4c75c587fe135e9db5352fa08f8ea404f
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-06 21:47:10 +0500

    Update Searchlocalrestore.sh

--- search-scripts/Searchlocalrestore.sh
+++ search-scripts/Searchlocalrestore.sh
@@ -9,22 +9,18 @@ if [[ "$USER_RESPONSE" != "yes" ]]; then
 fi
 
 # Automatically detect the latest timestamped backup folders
-SEARCH_MAIN_BACKUP_FOLDER=$(ls -d $PWD/search_main_backup_* 2>/dev/null | sort | tail -n 1)
-SEARCH_WL_BACKUP_FOLDER=$(ls -d $PWD/search_wl_backup_* 2>/dev/null | sort | tail -n 1)
+SEARCH_FINAL_BACKUP_FOLDER=$(ls -d $PWD/search_final_backup_folder_* 2>/dev/null | sort | tail -n 1)
+
 
 # Check if the directories exist
-if [ -z "$SEARCH_MAIN_BACKUP_FOLDER" ]; then
-  echo "Error: No MAIN BACKUP directory found in the current working directory."
+if [ -z "$SEARCH_FINAL_BACKUP_FOLDER" ]; then
+  echo "Error: No FINAL BACKUP directory found in the current working directory."
   exit 1
 fi
 
-if [ -z "$SEARCH_WL_BACKUP_FOLDER" ]; then
-  echo "Error: No WhiteLabel BACKUP directory found in the current working directory."
-  exit 1
-fi
 
-echo "Detected MAIN BACKUP folder: $SEARCH_MAIN_BACKUP_FOLDER"
-echo "Detected WL BACKUP folder: $SEARCH_WL_BACKUP_FOLDER"
+echo "Detected SEARCH FINAL BACKUP folder: $SEARCH_FINAL_BACKUP_FOLDER"
+
 
 # Define restoration paths
 HOME_WL="C:/xampp/htdocs/whitelabel"
@@ -42,15 +38,15 @@ MAIN_MDL_ADMIN="$HOME_MAIN/wp-content/themes/balance-theme/inc/edit/modules/edit
 
 # Copy files from the WhiteLabel backup
 echo "Restoring WhiteLabel files..."
-cp "$SEARCH_WL_BACKUP_FOLDER/whitelabel/public_html/assets/css/main.min.css" "$CSS_DIR"
-cp  "$SEARCH_WL_BACKUP_FOLDER/whitelabel/public_html/templates/" "$LS_RNDR_SEARCH"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" "$CSS_DIR"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/render-search-main-design.php" "$LS_RNDR_SEARCH"
 
 
 # MAIN files to restore
 echo "Restoring MAIN files..."
-cp "$SEARCH_MAIN_BACKUP_FOLDER/balancepro/wp-content/themes/balance-theme/css/" "$MAIN_CSS_DIR"
-cp "$SEARCH_MAIN_BACKUP_FOLDER/balancepro/public_html/wp-content/themes/balance-theme/inc/renderers/modules/" "$MAIN_RNDR_SEARCH"
-cp "$SEARCH_MAIN_BACKUP_FOLDER/balancepro/public_html/wp-content/themes/balance-theme/inc/edit/modules/" "$MAIN_MDL_ADMIN"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css" "$MAIN_CSS_DIR"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-search-main-design.php" "$MAIN_RNDR_SEARCH"
+cp  "$SEARCH_FINAL_BACKUP_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/edit-module-white-label-website-admins.php" "$MAIN_MDL_ADMIN"
 
 
 echo "Restore process completed!"
