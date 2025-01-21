4c045f61065b42daf00eb5829740d4e6364fb178
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-21 21:53:02 +0500

    Update partner-reports.php

--- balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/partner-reports.php
+++ balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/partner-reports.php
@@ -29,7 +29,7 @@ function partner_reports_page() {
      <!-- <div class="module-wrapper wlw_admins-module-module-wrapper-0" data-visible-on="tab-11">-->
     <div class="dropdown-wrapper">
     <b style="font-size:12px;">Partner Website:</b>
-    <input type="text" id="searchDropdown"  placeholder="Search websites..." onkeyup="filterDropdown()">
+    <input type="text" id="searchDropdown" style="width:30%;"  placeholder="Search websites..." onkeyup="filterDropdown()">
     <div class="dropdown-content" id="websiteDropdown" style="max-height: 200px; overflow-y: auto;">
         <?php
         if (!empty($wlw)) {
@@ -100,7 +100,7 @@ function partner_reports_page() {
 
 .dropdown-wrapper {
     position: relative;
-	width:50%;
+	
     
 }
 
@@ -109,7 +109,7 @@ function partner_reports_page() {
     overflow-y: auto;
     position: absolute;
     top: 100%;
-    width: 50%;
+    width: 38%;
     z-index: 999;
 }
 
