72fcb42670c38ee97fd48f4344bb567072737290
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-24 13:08:32 +0300

    Update SearchupdateFiles.sh

--- SearchupdateFiles.sh
+++ SearchupdateFiles.sh
@@ -37,17 +37,17 @@ MAIN_MDL_ADMIN="$HOME/domains/balancepro.org/public_html/wp-content/themes/balan
 
 echo "Copying MAIN updated files"
 
-cp "$SEARCH_FINAL_CLONE_FOLDER/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css" "$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
-cp "$SEARCH_FINAL_CLONE_FOLDER/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-search-main-design.php" "$MAIN_RNDR_SEARCH"
-cp "$SEARCH_FINAL_CLONE_FOLDER/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/edit-module-white-label-website-admins.php" "$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/edit-module-white-label-website-admins.php" 
+cp "$SEARCH_FINAL_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css" "$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/main.min_new.css"
+cp "$SEARCH_FINAL_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-search-main-design.php" "$MAIN_RNDR_SEARCH"
+cp "$SEARCH_FINAL_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/edit-module-white-label-website-admins.php" "$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/edit-module-white-label-website-admins.php" 
  
 
 
 
 echo "Copying WL files"
 
-cp "$SEARCH_FINAL_CLONE_FOLDER/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" "$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" 
-cp "$SEARCH_FINAL_CLONE_FOLDER/domains/whitelabel.balancepro.org/public_html/templates/render-search-main-design.php" "$LS_RNDR_SEARCH"
+cp "$SEARCH_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" "$HOME/domains/whitelabel.balancepro.org/public_html/assets/css/main.min.css" 
+cp "$SEARCH_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/render-search-main-design.php" "$LS_RNDR_SEARCH"
 
 
 
