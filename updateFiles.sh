6c4d1dedd169c26b536ecea0a75c7436412cad5a
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-28 11:31:20 +0500

    Update updateFiles.sh

--- updateFiles.sh
+++ updateFiles.sh
@@ -3,6 +3,9 @@
 SOURCE_DIR_MAIN="https://github.com/shehlizah/balancepro.git"
 SOURCE_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/"
 
+# Use the current directory as the source for custom files
+CUSTOM_SOURCE_DIR="$PWD"
+
 INC_SOURCE_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/includes/core/"    #other all 
 TMP_SOURCE_DIR="$HOME/domains/whitelabel.balancepro.org/public_html/templates/"
 MAIN_RNDR_SOURCE_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/"
@@ -11,8 +14,7 @@ LS_MAIN_SRC_DIR="$HOME/domains/balancepro.org/public_html/includes/core/"
 #BACKUP_DIR="/home/shahlizeh/finalChanges3Dec"   
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")  # Current timestamp for unique backups
 BACKUP_FOLDER="$HOME/backup_$TIMESTAMP"
-LS_FINAL_BRANCH_NAME="ls-final-deployment"
-LS_FINAL_CLONE_FOLDER="$PWD/ls_final_repo_${LS_FINAL_BRANCH_NAME}"
+
 # #echo "Source Directory: $SOURCE_DIR"
 # # echo "Backup Directory: $BACKUP_DIR"
 # #echo "Backup Folder: $BACKUP_FOLDER"
@@ -39,20 +41,20 @@ LS_MAIN_RMRS="${MAIN_RNDR_SOURCE_DIR}render-module-M14-15-resources-search.php"
 LS_MAIN_RSMDL="${MAIN_RNDR_SOURCE_DIR}render-search-main-design-lifestage.php"
 echo "Copying MAIN updated files"
 
-cp "$LS_MAIN_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/includes/core/search_query_pagination.php" "$LS_MAIN_SQ_PG" 
-cp "$LS_MAIN_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-module-M14-15-resources-search.php" "$LS_MAIN_SQ_PG" 
-cp "$LS_MAIN_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-search-main-design-lifestage.php" "$LS_MAIN_SQ_PG" 
+cp "$CUSTOM_SOURCE_DIR/balancepro/domains/balancepro.org/public_html/includes/core/search_query_pagination.php" "$LS_MAIN_SQ_PG" 
+cp "$CUSTOM_SOURCE_DIR/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-module-M14-15-resources-search.php" "$LS_MAIN_SQ_PG" 
+cp "$CUSTOM_SOURCE_DIR/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/renderers/modules/render-search-main-design-lifestage.php" "$LS_MAIN_SQ_PG" 
 
 
 
 echo "Copying WL files"
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/index.php" "$HOME/domains/whitelabel.balancepro.org/public_html/index.php"
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/config.php" "$HOME/domains/whitelabel.balancepro.org/public_html/includes/config.php"
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/search_query_pagination.php" "$LIFESTAGE" 
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/lifestage.php" "$WL_SQP"
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/ls.php" "$LS"
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/resource_search_pagination_content.php" "$LS_RS_SEARCH_PG_CONTENT"
-cp "$LS_FINAL_CLONE_FOLDER/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/core.php" "$LS_CORE"
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/index.php" "$HOME/domains/whitelabel.balancepro.org/public_html/index.php"
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/config.php" "$HOME/domains/whitelabel.balancepro.org/public_html/includes/config.php"
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/search_query_pagination.php" "$LIFESTAGE" 
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/lifestage.php" "$WL_SQP"
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/templates/ls.php" "$LS"
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/resource_search_pagination_content.php" "$LS_RS_SEARCH_PG_CONTENT"
+cp "$CUSTOM_SOURCE_DIR/whitelabel/domains/whitelabel.balancepro.org/public_html/includes/core/core.php" "$LS_CORE"
 
 
 
