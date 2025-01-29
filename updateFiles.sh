58335bde451641b5faa3d5db22d12ebfdd4abefc
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-01-29 17:17:47 +0500

    Update updateFiles.sh

--- updateFiles.sh
+++ updateFiles.sh
@@ -2,14 +2,14 @@
 
 SOURCE_DIR_MAIN="https://github.com/shehlizah/balancepro.git"
 
-
+# Use the current directory as the source for custom files
+CUSTOM_SOURCE_DIR="$PWD"
 
 MAIN_EDIT_SOURCE_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/"
 REPORTS_MAIN_SRC_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/"  
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")  # Current timestamp for unique backups
 BACKUP_FOLDER="$HOME/backup_$TIMESTAMP"
-REPORTS_ADMIN_BRANCH_NAME="reports-admin"
-REPORTS_ADMIN_CLONE_FOLDER="$PWD/reports_admin_repo_${REPORTS_ADMIN_BRANCH_NAME}"
+
 # #echo "Source Directory: $SOURCE_DIR"
 # # echo "Backup Directory: $BACKUP_DIR"
 # #echo "Backup Folder: $BACKUP_FOLDER"
@@ -22,9 +22,9 @@ REPORTS_ADMIN_EXP="${MAIN_EDIT_SOURCE_DIR}export.php"
 
 echo "Copying MAIN updated files"
 
-cp "$REPORTS_ADMIN_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/functions.php" "$REPORTS_ADMIN_FUNC" 
-cp "$REPORTS_ADMIN_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/partner-reports.php" "$REPORTS_ADMIN_PART" 
-cp "$REPORTS_ADMIN_CLONE_FOLDER/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/export.php" "$REPORTS_ADMIN_EXP" 
+cp "$CUSTOM_SOURCE_DIR/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/functions.php" "$REPORTS_ADMIN_FUNC" 
+cp "$CUSTOM_SOURCE_DIR/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/partner-reports.php" "$REPORTS_ADMIN_PART" 
+cp "$CUSTOM_SOURCE_DIR/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/export.php" "$REPORTS_ADMIN_EXP" 
 
 
 
