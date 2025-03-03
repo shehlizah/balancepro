1e9c1f3d21453ada0c6b72b11a36f3b270c5bad6
hali9242 <68986034+hali9242@users.noreply.github.com>
2025-03-03 19:22:51 +0500

    Update BackupScript.sh

--- BackupScript.sh
+++ BackupScript.sh
@@ -14,6 +14,7 @@
 SOURCE_DIR_MAIN="https://github.com/shehlizah/balancepro.git"
 BALANCEPRO_CSS_SOURCE_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/admin/"
 BALANCEPRO_FILE_SOURCE_DIR="$HOME/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/"
+BALANCEPRO_CHAT_SOURCE_DIR="$HOME/domains/balancepro.org/public_html/wp-admin/"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")  # Current timestamp for unique backups
 CHECKBOX_CSS="$PWD/checkbox_css_backup_$TIMESTAMP"
@@ -28,6 +29,7 @@ CHECKBOX_CSS="$PWD/checkbox_css_backup_$TIMESTAMP"
 #  Variables
 CHK_BX="${BALANCEPRO_CSS_SOURCE_DIR}admin.css"
 CHK_FL="${BALANCEPRO_FILE_SOURCE_DIR}edit-module-white-label-website-programs.php"
+CHK_CHAT="${BALANCEPRO_CHAT_SOURCE_DIR}chat.php"
 
 
 # # Check if source directory exists
@@ -46,6 +48,7 @@ echo "CHECKBOX_CSS created at: $CHECKBOX_CSS"
 # Ensure all necessary directories exist in the backup paths
 mkdir -p "$CHECKBOX_CSS/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/admin"
 mkdir -p "$CHECKBOX_CSS/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules"
+mkdir -p "$CHECKBOX_CSS/balancepro/domains/balancepro.org/public_html/wp-admin"
 
 echo "Created necessary directories under backup folders."
 
@@ -54,6 +57,7 @@ echo "Created necessary directories under backup folders."
 
 cp "$CHK_BX" "$CHECKBOX_CSS/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/css/admin/"
 cp "$CHK_FL" "$CHECKBOX_CSS/balancepro/domains/balancepro.org/public_html/wp-content/themes/balance-theme/inc/edit/modules/"
+cp "$CHK_CHAT" "$CHECKBOX_CSS/balancepro/domains/balancepro.org/public_html/wp-admin/"
 
 
 
